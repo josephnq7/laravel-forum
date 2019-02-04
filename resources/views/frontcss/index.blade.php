20b0c15625c7478d772f281fc9cf58b44965520c
Joseph <quanghiepk7xl@gmail.com>
2019-02-04 16:58:31 -0500

    modern css: styling header

---
+++ resources/views/frontcss/index.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CSS for Backend Devs</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans:400,400i,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="/css/front/normalize.css">
+    <link rel="stylesheet" href="/css/front/app.css">
+
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-top">
+                <h1>XCasts</h1>
+                <a href="#">Sign In</a>
+            </div>
+            <nav>
+                <a href="#">Catalog</a>
+                <a href="#">Series</a>
+                <a href="#">Podcast</a>
+                <a href="#">Discussions</a>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/frontcss', 'FrontCssController@index');
 
 Route::get('/threads', 'ThreadController@index');
 Route::get('/threads/{thread}', 'ThreadController@show');
